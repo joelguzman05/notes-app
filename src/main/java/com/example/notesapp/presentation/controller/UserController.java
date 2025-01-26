4df84e19e2dad08f88bb966a2fd1e434b352c6a7
Joel <ing.joel.guzman.05@gmail.com>
2025-01-25 23:59:36 -0400

    Add Swagger documentation to UserController for retrieving user profile

--- src/main/java/com/example/notesapp/presentation/controller/UserController.java
+++ src/main/java/com/example/notesapp/presentation/controller/UserController.java
@@ -2,6 +2,9 @@ package com.example.notesapp.presentation.controller;
 
 import com.example.notesapp.domain.service.UserService;
 import com.example.notesapp.presentation.response.dto.UserProfileResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -12,12 +15,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
+@Tag(name = "User", description = "User profile management API")
 @RequestMapping("/user")
 public class UserController {
 
     private final UserService userService;
 
     @GetMapping("/me")
+    @Operation(
+            summary = "Get authenticated user",
+            description = "Returns the currently authenticated user's profile.",
+            security = @SecurityRequirement(name = "Bearer Authentication")
+    )
     public ResponseEntity<UserProfileResponse> getAuthenticatedUser(@AuthenticationPrincipal UserDetails userDetails) {
         UserProfileResponse user = userService.getAuthenticatedUser(userDetails);
         return ResponseEntity.ok(user);
