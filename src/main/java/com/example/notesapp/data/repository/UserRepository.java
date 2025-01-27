0979e1feedbd78b5fc7ef49136bfcc5d0ee019d7
Joel <ing.joel.guzman.05@gmail.com>
2025-01-26 22:54:20 -0400

    Remove unnecessary @Repository annotation from UserRepository

--- src/main/java/com/example/notesapp/data/repository/UserRepository.java
+++ src/main/java/com/example/notesapp/data/repository/UserRepository.java
@@ -2,11 +2,9 @@ package com.example.notesapp.data.repository;
 
 import com.example.notesapp.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
